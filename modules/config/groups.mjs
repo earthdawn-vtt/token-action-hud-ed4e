f54249daaad16963a605d8e6a25357724d7b32c9
Chris S. D. <christopherDick00@gmail.com>
2026-08-25 19:01:18 +0200

    feat: add group configurations

---
+++ modules/config/groups.mjs
@@ -0,0 +1,113 @@
+/**
+ * @import { HUDGroup } from "../layout/defaults.mjs";
+ */
+
+/**
+ * Module-specific categories that exist on the HUD as top level.
+ */
+export const CATEGORY_IDS = [
+  "general",
+  "powers",
+  "talents",
+  "skills",
+  "devotions",
+  "spells",
+  "inventory",
+  "combat",
+  "effects",
+  "utility",
+];
+
+/**
+ * Module-only group IDs that do not exist directly in CONFIG.ED4E.
+ */
+export const MODULE_GROUP_IDS = [
+  "attributes",
+  "other",
+  "favorites",
+  "utility",
+];
+
+/**
+ * Group data for module-specific groups.
+ * @returns { Record<string, HUDGroup> }
+ */
+export function getModuleGroups() {
+  const moduleGroups = {};
+  for ( const group of MODULE_GROUP_IDS ) {
+    moduleGroups[ group ] = {
+      id:   group,
+      name: _loc( `TokenActionHud.Groups.Names.${ group }` ),
+      type: "system",
+    };
+  }
+  return moduleGroups;
+}
+
+/**
+ * Convert ED4E config to groups.
+ * @param { object } ed4eConfig
+ * @returns { HUDGroup[] }
+ */
+function ed4eConfigToGroups( ed4eConfig ) {
+  return Object.entries( ed4eConfig ).map( ( [ key, value ] ) => {
+    return {
+      id:   key,
+      name: value?.label ?? value?.name ?? value,
+      type: "system",
+    };
+  } );
+}
+
+/**
+ * Group data for groups based on ED4E config data.
+ * @returns { Record<string, HUDGroup[]> }
+ */
+export function getEd4eGroups() {
+  const ed4eConfig = CONFIG.ED4E;
+  return {
+    actionSpeed: ed4eConfigToGroups( ed4eConfig.ACTIONS.action ),
+    // TODO: make (non-default) groups for rolltypes
+  };
+}
+
+/**
+ * Basic group data for top-level categories.
+ * @returns {Record<string, HUDGroup>}
+ */
+export function getCategoryBaseGroups() {
+  const categoryGroups = CATEGORY_IDS.map( id => {
+    return {
+      id,
+      name: _loc( `TokenActionHud.Categories.Names.${ id }` ),
+      type: "system",
+    };
+  } );
+  return Object.fromEntries( categoryGroups.map( group => [ group.id, group ] ) );
+}
+
+export function getCategoryHierarchy() {
+  const systemGroups = getSystemGroups();
+  const ed4eGroups = getEd4eGroups();
+  return {
+    general:   [ systemGroups.attributes , systemGroups.other, ],
+    powers:    [],
+    talents:   [ ...ed4eGroups.actionSpeed ],
+    skills:    [ ...ed4eGroups.actionSpeed ],
+    devotions: [],
+    spells:    [],
+    inventory: [],
+    combat:    [],
+    effects:   [],
+    utility:   [],
+  };
+}
+
+export function getSystemGroups() {
+  const moduleGroups = getModuleGroups();
+  const ed4eGroups = getEd4eGroups();
+  return {
+    ...moduleGroups,
+    ...Object.fromEntries( Object.values( ed4eGroups ).flat().map( group => [ group.id, group ] ) ),
+  };
+}
