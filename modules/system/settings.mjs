8c2b5a3f35369df9f7eb8a39514ed107667d4c77
Chris S. D. <christopherDick00@gmail.com>
2026-08-28 18:09:02 +0200

    feat: add register function to handle module settings

---
+++ modules/system/settings.mjs
@@ -0,0 +1,8 @@
+/**
+ * Register all module settings with Foundry.
+ * @param {function} onChangeFunction This function can be called in the registered settings onChange parameter
+ * to trigger Token Action HUD Core's own settings update process when a TAH system setting is updated.
+ */
+export function register( onChangeFunction ) {
+
+}
