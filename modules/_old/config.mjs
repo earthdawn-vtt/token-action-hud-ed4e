ed51cfee44b7824513d7bf47fa9b71039bc27c12
Chris S. D. <christopherDick00@gmail.com>
2026-08-28 19:57:45 +0200

    refactor: remove old code

--- modules/_old/config.mjs
+++
@@ -1,9 +0,0 @@
-export const groups = {
-  other: { id: "other", type: "system" },
-};
-
-export const categories = {
-  id:     "general",
-  name:   _loc( "TokenActionHud.Categories.Names.general" ),
-  groups: [ "attributes", "other", "system" ],
-};
